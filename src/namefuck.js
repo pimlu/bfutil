37d83a0a479b56c1ee078d706d465d75d42d1c76
pimlu <pimlu@users.noreply.github.com>
2015-12-02 13:42:04 -0500

    add else, more efficient tmp usage

--- src/namefuck.js
+++ src/namefuck.js
@@ -138,7 +138,7 @@ function mov(state, a, b) {
   return '{0}[-]{1}[{0}+{1}-]'.format(a, b);
 }
 function cop(state, a, b) {
-  var tmp = gtmp(state);
+  var tmp = 'coptmp';
   return '{0}[-]{1}[{0}+{2}+{1}-]{2}[{1}+{2}-]'.format(a, b, tmp);
 }
 function whi(state, a) {
@@ -146,22 +146,33 @@ function whi(state, a) {
   return '{0}['.format(a);
 }
 function mif(state, a) {
-  var tmp1 = gtmp(state);
-  var tmp2 = gtmp(state);
+  var tmp1 = 'iftmp1'+state.ctrl.length;
+  var tmp2 = 'iftmp2'+state.ctrl.length;
   state.ctrl.push(['if', tmp1])
   return ('{0}[{1}+{2}+{0}-]'+
           '{2}[{0}+{2}-]'+
           '{1}[').format(a, tmp1, tmp2);
 }
+function melse(state) {
+  var tmp = 'elsetmp'+state.ctrl.length;
+  if(state.ctrl[state.ctrl.length-1][0] !== 'if') {
+    throw new Exception('else without if');
+  }
+  var out = '{0}-'+end(state)+'{0}+[';
+  state.ctrl.push(['else', tmp]);
+  return out.format(tmp);
+}
 function end(state) {
   var ctrl = state.ctrl.pop();
-  window.kek={ctrl:console.log(ctrl), };
-  if(ctrl[0] === 'whi') {
+  var name = ctrl[0]
+  if(name === 'whi') {
     return '{0}]'.format(ctrl[1]);
-  } else if(ctrl[0] === 'if') {
+  } else if(name === 'if') {
     return ('{0}[-]'+
             ']'+
             '{0}[-]').format(ctrl[1]);
+  } else if(name === 'else') {
+    return '{0}-]'.format(ctrl[1]);
   }
 }
 
@@ -170,6 +181,7 @@ var ms = namefuck.ms = {
   cop: cop,
   whi: whi,
   if: mif,
+  else: melse,
   end: end
 };
 
